630630a8c0d2a51752c6b62d725839a7e07f0fe6
Misha Skyda <mihail.skida@kartenmacherei.de>
2016-05-13 12:36:18 +0200

    calculate distance by coords

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,57 @@
+function map(SpotsModel){
+
+	function showMap(position) {
+
+		var lat = position.coords ? position.coords.latitude : position.lat,
+			lng = position.coords ? position.coords.longitude : position.lng;
+
+		var map = new google.maps.Map(document.getElementById("map"), {
+			center:new google.maps.LatLng(lat, lng),
+			zoom: 13,
+			mapTypeId:google.maps.MapTypeId.ROADMAP
+		});
+
+		var userMarker = new google.maps.Marker({
+			position: {lat: lat, lng: lng},
+			map: map,
+			icon: './img/user.png'
+		});
+
+		SpotsModel.updateSpots({lat: lat, lng: lng});
+
+	}
+
+	jQuery('.location-controls .geolocation').click(function(e){
+
+		e.preventDefault();
+
+		navigator.geolocation.getCurrentPosition(showMap);
+
+	});
+
+	jQuery('.location-controls .geocoding').click(function(e){
+
+		e.preventDefault();
+
+		var address = jQuery('.location-controls .address').val();
+
+		if(!address) return; // todo input validation
+
+		jQuery.ajax({
+			method: "POST",
+			url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + 'AIzaSyDW3irgXC2Ogys9XTVV8oaJ6lXbpNTTap0'
+		}).done(function(resp){
+
+			// todo: results selection
+
+			showMap(resp.results[0].geometry.location);
+
+		});
+
+	});
+
+}
+
 function newClient(){
 
 	var app = angular.module('app', []);
@@ -5,14 +59,16 @@ function newClient(){
 	app.controller('Ctrl', ['SpotsModel', '$scope', function (SpotsModel, $scope) {
 
 		_.extend(this, {
-			remove: function(id){SpotsModel.removeSpot(id);}
+			removeSpot: function(id){SpotsModel.removeSpot(id);},
+			goToSpot: function(coords){SpotsModel.goToSpot(coords);},
+			getDistance: function(coords){return SpotsModel.getDistance(coords);}
 		});
 
 		$scope.spots = SpotsModel.spots;
 
 		$scope.$on('update:spots', function(){$scope.$apply(function () {$scope.spots = SpotsModel.spots});});
 
-		SpotsModel.updateSpots();
+		map(SpotsModel);
 
 	}]);
 
@@ -22,7 +78,10 @@ function newClient(){
 
 			spots: [],
 
-			updateSpots: function(){
+			updateSpots: function(uCoords){
+
+				this.uCoords = uCoords || this.uCoords || false;
+
 				jQuery.ajax({
 					method: "GET",
 					url: "/api/spots"
@@ -30,6 +89,7 @@ function newClient(){
 					this.spots = resp;
 					$rootScope.$broadcast('update:spots');
 				}, this));
+
 			},
 
 			removeSpot: function(id){
@@ -37,8 +97,29 @@ function newClient(){
 					method: "DELETE",
 					url: "/api/spot/" + id
 				}).done(jQuery.proxy(this.updateSpots, this));
+			},
+
+			getDistance: function(coords){
+
+				var R = 6371, // earth’s radius in kilometres
+					φ1 = coords.lat * Math.PI / 180,
+					φ2 = this.uCoords.lat * Math.PI / 180,
+					Δφ = (this.uCoords.lat - coords.lat) * Math.PI / 180,
+					Δλ = (this.uCoords.lng - coords.lng) * Math.PI / 180;
+
+				var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ/2) * Math.sin(Δλ/2);
+
+				return (R * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))).toFixed(1);
+
+			},
+
+			goToSpot: function(){
+
+				console.log(arguments);
+
 			}
-		}
+
+		};
 
 		return SpotsModel;
 
@@ -54,9 +135,9 @@ function oldClient(){
 	var client = {
 
 		mockup: {
-			name: 'john smith',
-			desc: 'regular guy blabla',
-			coords: {lat: 53.551202, lon: 10.002056}
+			name: 'cool place',
+			desc: 'blabla',
+			coords: {lat: +(180 *  Math.random()).toFixed(6), lng: +(180 *  Math.random()).toFixed(6)}
 		},
 
 		init: function(){
@@ -79,11 +160,11 @@ function oldClient(){
 			});
 		},
 
-		create: function(data){
+		create: function(){
 			jQuery.ajax({
 				method: "POST",
 				url: "/api/spot",
-				data: _.extend({}, this.mockup, data)
+				data: this.mockup,
 			}).done(jQuery.proxy(this.refresh, this));
 		},
 
@@ -147,76 +228,11 @@ function oldClient(){
 
 	client.init();
 
-	jQuery('.open-bar').click(function(e){
-
-		e.preventDefault();
-
-		jQuery('.create-bar').show();
-
-	});
-
 	jQuery('.create').click(function(e){
 
 		e.preventDefault();
 
-		var data = {name: jQuery('.name').val() || 'john smith'}; // serialize
-
-		client.create(data);
-
-		jQuery('.create-bar').hide();
-
-	});
-
-	jQuery('.refresh').click(function(e){
-		e.preventDefault();
-		client.refresh();
-	});
-
-}
-
-function map(){
-
-	function showMap(position) {
-
-		var lat = position.coords ? position.coords.latitude : position.lat,
-			lng = position.coords ? position.coords.longitude : position.lng;
-
-		var Map = new google.maps.Map(document.getElementById("map"), {
-			center:new google.maps.LatLng(lat, lng),
-			zoom: 13,
-			mapTypeId:google.maps.MapTypeId.ROADMAP
-		});
-
-		jQuery('.closest-spots').removeClass('hide');
-
-	}
-
-	jQuery('.location-controls .geolocation').click(function(e){
-
-		e.preventDefault();
-
-		navigator.geolocation.getCurrentPosition(showMap);
-
-	});
-
-	jQuery('.location-controls .geocoding').click(function(e){
-
-		e.preventDefault();
-
-		var address = jQuery('.location-controls .address').val();
-
-		if(!address) return; // todo input validation
-
-		jQuery.ajax({
-			method: "POST",
-			url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + 'AIzaSyDW3irgXC2Ogys9XTVV8oaJ6lXbpNTTap0'
-		}).done(function(resp){
-
-			// todo: results selection
-
-			showMap(resp.results[0].geometry.location);
-
-		});
+		client.create();
 
 	});
 
@@ -224,4 +240,3 @@ function map(){
 
 newClient();
 oldClient();
-map();
