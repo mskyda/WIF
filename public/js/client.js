8213233d0d71eae5b7953f472c35c086ae8b5379
Misha Skyda <mihail.skida@kartenmacherei.de>
2016-05-25 15:56:15 +0200

    fix for url-states

--- public/js/client.js
+++ public/js/client.js
@@ -1,19 +1,22 @@
 angular.module('wif',['ui.router','ngResource', 'ngCookies', 'controllers','services']).config(function($stateProvider){
 
-	$stateProvider.state('search',{
-		url:'search',
-		templateUrl:'tpl/search.tpl',
-		controller:'SearchPageController'
-	}).state('manage',{
-	   url:'manage',
-	   templateUrl:'tpl/manage.tpl',
-	   controller:'ManagePageController'
-	}).state('about',{
-		url:'about',
-		templateUrl:'tpl/about.tpl',
-		controller:'AboutPageController'
-	});
+	$stateProvider
+		.state('search',{
+			url:'/search',
+			templateUrl:'tpl/search.tpl',
+			controller:'SearchPageController'
+		}).state('manage',{
+		   url:'/manage',
+		   templateUrl:'tpl/manage.tpl',
+		   controller:'ManagePageController'
+		}).state('about',{
+			url:'/about',
+			templateUrl:'tpl/about.tpl',
+			controller:'AboutPageController'
+		});
 
-}).run(function($state){$state.go('search')});
+}).run(function($state){
+	$state.go('search');
+});
 
 angular.bootstrap(document.documentElement, ['wif']);
