adae6b5e0b2469b0003086c38fbf71e6d1f509de
Misha Skyda <mihail.skida@gmail.com>
2016-06-20 10:51:07 +0200

    redirect from www to non-www

--- server.js
+++ server.js
@@ -11,14 +11,24 @@ var express         = require('express'),
 		ca:  [fs.readFileSync('./private/ssl/crt_int.crt', 'utf8')]
 	},
 	app             = express(),
-	httpServer      = http.createServer(app);
-	httpsServer     = https.createServer(credentials, app);
+	httpServer      = http.createServer(app),
+	httpsServer     = https.createServer(credentials, app),
+	wwwRedirect     = function(req, res, next){
+		if(req.headers.host.slice(0, 4) === 'www.'){
+			var newHost = req.headers.host.slice(4);
+			return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+		}
+		next();
+	};
 
 // Todo: config.js
+
+app.set('trust proxy', true);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(forceSSL);
+app.use(wwwRedirect);
 app.use(express.static('./public'));
 
 app.get('/api/spots', api.Spots.get);
@@ -28,16 +38,16 @@ app.put('/api/spots/:id', api.Spots.put);
 app.delete('/api/spots/:id', api.Spots.delete);
 app.post('/api/auth', api.Auth.init);
 
-app.get('*', function(req, res){ // 404
-	res.status(404);
-	console.log('Error: Not found URL ',req.url);
-	res.send({ error: 'Not found' });
-});
-
 app.use(function(err, req, res){ // error
 	res.status(err.status || 500);
 	console.log('Error: ', res.statusCode ,err.message);
 	res.send({ error: err.message });
+});
+
+app.get('*', function(req, res){ // 404
+	res.status(404);
+	console.log('Error: Not found URL ',req.url);
+	res.send({ error: 'Not found' });
 });
 
 var portHTTP = process.env.PORT_HTTP || 8080,
