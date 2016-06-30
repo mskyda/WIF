fe566ebf282f88118c4d7518d52d9c36b6240577
Misha Skyda <mihail.skida@gmail.com>
2016-06-30 16:21:26 +0200

    decouple i18n from translations-logic

--- public/js/rconfig.js
+++ public/js/rconfig.js
@@ -1,22 +1,24 @@
 require.config({
 	baseUrl: "/js",
 	paths: {
-		'angular'    : '3p/angular/angular.min',
-		'ngResource' : '3p/angular-resource/angular-resource.min',
-		'ngCookies'  : '3p/angular-cookies/angular-cookies.min',
-		'uiRouter'   : '3p/angular-ui-router/release/angular-ui-router.min',
-		'translate'  : '3p/angular-translate/angular-translate.min'
+		'angular'         : '3p/angular/angular.min',
+		'ngResource'      : '3p/angular-resource/angular-resource.min',
+		'ngCookies'       : '3p/angular-cookies/angular-cookies.min',
+		'uiRouter'        : '3p/angular-ui-router/release/angular-ui-router.min',
+		'translate'       : '3p/angular-translate/angular-translate.min',
+		'translateLoader' : '3p/angular-translate-loader-static-files/angular-translate-loader-static-files.min'
 	},
 	shim: {
-		'translate'    : {deps: ['angular']},
-		'uiRouter'     : {deps: ['angular']},
-		'ngResource'   : {deps: ['angular']},
-		'ngCookies'    : {deps: ['angular']},
-		'translations' : {deps: ['translate', 'ngCookies']},
-		'router'       : {deps: ['uiRouter']},
-		'services'     : {deps: ['ngResource']},
-		'controllers'  : {deps: ['ngCookies']},
-		'client'       : {deps: ['router', 'controllers', 'services', 'translations']}
+		'translate'       : {deps: ['angular']},
+		'uiRouter'        : {deps: ['angular']},
+		'ngResource'      : {deps: ['angular']},
+		'ngCookies'       : {deps: ['angular']},
+		'translateLoader' : {deps: ['translate']},
+		'translations'    : {deps: ['translateLoader']},
+		'router'          : {deps: ['uiRouter']},
+		'services'        : {deps: ['ngResource']},
+		'controllers'     : {deps: ['ngCookies']},
+		'client'          : {deps: ['router', 'controllers', 'services', 'translations']}
 	}
 });
 
