cd7ed99928372592415b50527cf426068bb29b74
Misha Skyda <mihail.skida@gmail.com>
2016-07-30 13:44:33 +0200

    working on minification of JS

--- gulp.js
+++ gulp.js
@@ -19,8 +19,9 @@
 	gulp.task('lint', function () {
 		return gulp.src([
 			'**/*.js',
+			'!public/dev/js/3p/**/*.js',
 			'!node_modules/**/*.js',
-			'!public/js/3p/**/*.js'
+			'!public/dist/**/*.js'
 		])
 			.pipe(glp.eslint())
 			.pipe(glp.eslint.format())
@@ -29,15 +30,69 @@
 
 	gulp.task('js', function (cb) {
 
-		/* glp.requirejs.optimize({
-			//preserveLicenseComments: false,
-			appDir: staticPath + '/js',
-			dir: webPath + '/js',
-			mainConfigFile: staticPath + '/js/require-config.js',
-			baseUrl: '../js'
-		}, cb()); */
+		require('requirejs').optimize({
+			optimize: 'uglify2', // method
+			appDir  : 'public/dev/js', // input
+			baseUrl : '.', // path
+			dir     : 'public/dist/js', // output
 
-		cb();
+			paths: {
+				'angular'        : '3p/angular/angular.min',
+				'ngResource'     : '3p/angular-resource/angular-resource.min',
+				'ngCookies'      : '3p/angular-cookies/angular-cookies.min',
+				'uiRouter'       : '3p/angular-ui-router/release/angular-ui-router.min',
+				'ngTranslate'    : '3p/angular-translate/angular-translate.min',
+				'translateLoader': '3p/angular-translate-loader-static-files/angular-translate-loader-static-files.min'
+			},
+			shim: {
+				'ngTranslate'    : { deps: ['angular']},
+				'uiRouter'       : { deps: ['angular']},
+				'ngResource'     : { deps: ['angular']},
+				'ngCookies'      : { deps: ['angular']},
+				'translateLoader': { deps: ['ngTranslate']},
+				'translate'      : { deps: ['translateLoader']},
+				'router'         : { deps: ['uiRouter']},
+				'services'       : { deps: ['ngResource']},
+				'controllers'    : { deps: ['ngCookies']},
+				'client'         : { deps: [ 'router', 'controllers', 'services', 'translate' ]}
+			},
+
+			removeCombined: true, // remove files which were nested
+
+			findNestedDependencies: true, // deep check for deps
+
+			modules: [{
+				name: 'rconfig'
+				/* include: ['3p/require'] */
+			}],
+
+			uglify2: {
+				output  : { beautify: false },
+				compress: {
+					sequences    : true,  // join consecutive statemets with the “comma operator”
+					properties   : true,  // optimize property access: a["foo"] → a.foo
+					dead_code    : true,  // discard unreachable code
+					drop_debugger: true,  // discard “debugger” statements
+					unsafe       : false, // some unsafe optimizations
+					conditionals : true,  // optimize if-s and conditional expressions
+					comparisons  : true,  // optimize comparisons
+					evaluate     : true,  // evaluate constant expressions
+					booleans     : true,  // optimize boolean expressions
+					loops        : true,  // optimize loops
+					unused       : true,  // drop unused variables/functions
+					hoist_funs   : true,  // hoist function declarations
+					hoist_vars   : false, // hoist variable declarations
+					if_return    : true,  // optimize if-s followed by return/continue
+					join_vars    : true,  // join var declarations
+					cascade      : true,  // try to cascade `right` into `left` in sequences
+					side_effects : true,  // drop side-effect-free statements
+					warnings     : false  // warn about potentially dangerous optimizations/code
+				},
+				warnings: false,
+				mangle  : true // uglify (shorten) names of vars and params
+			}
+
+		}, function(){ cb(); });
 
 	});
 
